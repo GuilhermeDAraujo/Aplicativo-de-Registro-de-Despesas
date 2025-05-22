be86f2b180cbd34d1d382290f4a613e637522bd7
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2025-05-22 11:47:31 -0300

    Criando e configurando a model Lancamento(Banco de dados)

--- backend/lancamentos/models.py
+++ backend/lancamentos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Lancamento(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    banco = models.CharField(max_length=100)
+    historico = models.TextField(blank=False, null=False)
+    
+    def __str__(self):
+        return self.nome
