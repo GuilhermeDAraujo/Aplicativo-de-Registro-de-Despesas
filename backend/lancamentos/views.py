f84140a5231a7d60200502a3032049cf822859dd
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2025-05-25 10:15:09 -0300

    Criando api GET na view lancamentos

--- backend/lancamentos/views.py
+++ backend/lancamentos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.responde import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Lancamento
+from .serializers import LancamentoSerializer
+
+@api_view(['GET'])
+def lancamento_view(request):
+    if request.method == 'GET':
+        lancamento = Lancamento.objects.all()
+        serializer = LancamentoSerializer(lancamento, many=True)
+        return Response(serializer.data)
