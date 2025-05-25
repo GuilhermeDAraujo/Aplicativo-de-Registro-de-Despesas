dcbeea3fc92a7ad4d7ea81bb9520b918561b0822
Guilherme Dias Araujo <guilherme.araujo97@outlook.com>
2025-05-25 12:02:20 -0300

    Configurando a page Home para receber os lancamentos já registrados e mostrar na tela

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,14 +1,25 @@
-import React, {useEffect, useState, useRef} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './style.css'
 import Trash from '../../assets/trash.png'
 
 function Home() {
-  const[lancamentos, setLancamentos] = useState([]);
+  const [lancamentos, setLancamentos] = useState([]);
 
   const inputNome = useRef()
   const inputValor = useRef()
   const inputBanco = useRef()
   const inputHistorico = useRef()
+
+  useEffect(() => {
+    buscarLancamentos();
+  }, [])
+
+  const buscarLancamentos = () => {
+    fetch('http://localhost:8000/lancamento/lancamentos/')
+      .then(response => response.json())
+      .then(data => setLancamentos(data))
+      .catch(err => console.error('Erro ao buscar lançamentos:', err));
+  }
 
   const registrarLancamento = async () => {
     const novoLancamento = {
@@ -18,9 +29,9 @@ function Home() {
       historico: inputHistorico.current.value
     };
 
-    await fetch('http://localhost:8000/lancamento/lancamentos/',{
+    await fetch('http://localhost:8000/lancamento/lancamentos/', {
       method: 'POST',
-      headers:{
+      headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(novoLancamento)
@@ -37,13 +48,30 @@ function Home() {
       <div className='from-wrapper'>
         <form>
           <h1>Registrar Despesa</h1>
-          <input placeholder='Nome' name='nome' type='text' ref={inputNome}/>
-          <input placeholder='Valor' name='valor' type='decimal' ref={inputValor}/>
-          <input placeholder='Banco' name='banco' type='text' ref={inputBanco}/>
-          <input placeholder='Histórico' name='historico' type='text' ref={inputHistorico}/>
+          <input placeholder='Nome' name='nome' type='text' ref={inputNome} />
+          <input placeholder='Valor' name='valor' type='decimal' ref={inputValor} />
+          <input placeholder='Banco' name='banco' type='text' ref={inputBanco} />
+          <input placeholder='Histórico' name='historico' type='text' ref={inputHistorico} />
           <button type='button' onClick={registrarLancamento}>Registrar</button>
         </form>
       </div>
+
+      <h1>Despesas já registradas</h1>
+      {lancamentos.map(item => (
+        <div key={item.id} className='card'>
+          <div>
+            <div>
+              <p>Nome: <span>{item.nome}</span></p>
+              <p>Valor: <span>R${item.valor}</span></p>
+              <p>Banco: <span>{item.banco}</span></p>
+              <p>Histórico: <span>{item.historico}</span></p>
+            </div>
+            <button>
+              <img src={Trash} className="img-lixeira" />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   )
 
